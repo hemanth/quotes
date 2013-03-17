118a0f8c3eb5a486338c7517e3987cbce4260f9f
Hemanth.HM <hemanth.hm@gmail.com>
2013-03-17 13:05:11 +0530

    First cut

---
+++ random.js
@@ -0,0 +1,10 @@
+var quotes = new XMLHttpRequest();
+quotes.open('GET', 'quotes', false);
+quotes.send(null);
+var wisdom = quotes.responseText.split('\n');
+
+quote=document.getElementById('quote')
+
+setInterval(function(){
+quote.innerText=wisdom[Math.floor(Math.random() * wisdom.length)]
+},6000);
